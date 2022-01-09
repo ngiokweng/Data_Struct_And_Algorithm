d89a4e0dcbc7ac4e00de7333ddbc12f610ff9888
NgIokWeng <2334439391@qq.com>
2022-01-09 10:53:19 +0800

    選擇排序2：堆排序

--- 排序/排序/選擇排序.cpp
+++ 排序/排序/選擇排序.cpp
@@ -30,3 +30,41 @@ void SelectSort(SqList& L) {
 		}
 	}
 }
+/*選擇排序2：堆排序*/
+void HeapAdjust(SqList& L, int s, int m) {
+	/*已合L.r[s..m]中記錄的關鍵字除L.r[s]之外均滿足堆的定義，
+		本函數調整L.r[s]的關鍵字，使L.r[s..m]成為一個大根堆
+	*/
+	RedType rc = L.r[s];
+	for (int j = 2 * s; j <= m; j *= 2) {
+		if (j < m && L.r[j].key < L.r[j + 1].key)++j;
+		if (rc.key >= L.r[j].key)break;
+		L.r[s] = L.r[j]; s = j;  //rc應插在位置s上
+	}
+	L.r[s] = rc; //插入
+}
+
+void HeapSort(SqList& L) {
+	int i;
+	for (i = L.length / 2; i >= 1; i--)
+		HeapAdjust(L, i, L.length);
+	for (i = L.length; i > 1; i--) {
+		RedType tmp = L.r[1];
+		L.r[1] = L.r[i];
+		L.r[i] = tmp;
+		HeapAdjust(L, 1, i - 1);
+	}
+}
+//for test
+int main() {
+	SqList L;
+	L.length = 0;
+	for (int i = 1; i < 5; i++) {
+		cin >> L.r[i].key;
+		L.length++;
+	}
+	HeapSort(L);
+	for (int i = 1; i < 5; i++) {
+		cout << L.r[i].key << " ";
+	}
+}
